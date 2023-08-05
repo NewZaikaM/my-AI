d27a5e46f6bbc3f548355fa10fb3381402c65598
Maxim Zaika <rabotzai@gmail.com>
2023-08-05 08:28:27 +0200

    added the section Demo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 				<img src={logo} alt="samz_logo" className="w-28 object-contain" />
 				<button
 					type="button"
-					onClick={() => window.open('https://www.google.com')}
+					onClick={() => window.open('https://github.com/NewZaikaM/my-AI')}
 					className="black_btn"
 				>
 					GitHub
